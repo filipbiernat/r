0f73d04a302776a0bb17336577b791180350071c
filipbiernat <fbiernat@gmail.com>
2025-02-20 23:12:58 +0100

    Read CSV files from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import TableView from "./components/TableView";
 
+const GITHUB_API_URL =
+    "https://api.github.com/repos/filipbiernat/RScraper/contents/data";
+
 const App: React.FC = () => {
+    const [csvFiles, setCsvFiles] = useState<string[]>([]);
+
+    useEffect(() => {
+        fetch(GITHUB_API_URL)
+            .then((response) => response.json())
+            .then((data) => {
+                const csvFiles = data
+                    .filter((file: any) => file.name.endsWith(".csv"))
+                    .map((file: any) => file.name);
+                setCsvFiles(csvFiles);
+            });
+    }, []);
+
     return (
         <Router>
             <div style={{ display: "flex" }}>
-                <Sidebar />
+                <Sidebar csvFiles={csvFiles} />
                 <div style={{ marginLeft: "200px", padding: "20px" }}>
                     <Routes>
-                        <Route
-                            path="/table/Hiszpanskie_El_Clasico__Katowice__1os"
-                            element={
-                                <TableView filePath="/src/data/Hiszpanskie_El_Clasico__Katowice__1os.csv" />
-                            }
-                        />
-                        <Route
-                            path="/table/Living_in_America__Warszawa__1os"
-                            element={
-                                <TableView filePath="/src/data/Living_in_America__Warszawa__1os.csv" />
-                            }
-                        />
+                        {csvFiles.map((file) => (
+                            <Route
+                                key={file}
+                                path={`/table/${file.replace(".csv", "")}`}
+                                element={
+                                    <TableView
+                                        filePath={`https://raw.githubusercontent.com/filipbiernat/RScraper/master/data/${file}`}
+                                    />
+                                }
+                            />
+                        ))}
                     </Routes>
                 </div>
             </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Sidebar: React.FC = () => {
+interface SidebarProps {
+    csvFiles: string[];
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ csvFiles }) => {
     return (
         <div>
             <ul>
-                <li>
-                    <Link to="/table/Hiszpanskie_El_Clasico__Katowice__1os">
-                        Hiszpanskie_El_Clasico__Katowice__1os
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/table/Living_in_America__Warszawa__1os">
-                        Living_in_America__Warszawa__1os
-                    </Link>
-                </li>
-                {/* Dodaj więcej linków do innych tabel w razie potrzeby */}
+                {csvFiles.map((file) => (
+                    <li key={file}>
+                        <Link to={`/table/${file.replace(".csv", "")}`}>
+                            {file.replace(".csv", "")}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
