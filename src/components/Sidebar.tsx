94a6e7ec0df6fe50a0e23c1854bdc5cfc3c4aa8a
filipbiernat <fbiernat@gmail.com>
2025-02-21 01:50:45 +0100

    react-pro-sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { convertPolishChars } from "../utils/convertPolishChars";
 import { parseFileName } from "../utils/parseFileName";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import "react-pro-sidebar/dist/css/styles.css";
 
 interface SidebarProps {
     csvFiles: string[];
@@ -9,20 +12,21 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ csvFiles }) => {
     return (
-        <div>
-            <ul>
+        <ProSidebar>
+            <Menu iconShape="square">
                 {csvFiles.map((file) => {
                     const fileNameWithoutSuffix = file.replace(".csv", "");
                     const displayName = parseFileName(fileNameWithoutSuffix);
                     const pathName = convertPolishChars(fileNameWithoutSuffix);
                     return (
-                        <li key={file}>
-                            <Link to={`/${pathName}`}>{displayName}</Link>
-                        </li>
+                        <MenuItem key={file}>
+                            <Typography>{displayName}</Typography>
+                            <Link to={`/${pathName}`} />
+                        </MenuItem>
                     );
                 })}
-            </ul>
-        </div>
+            </Menu>
+        </ProSidebar>
     );
 };
 
